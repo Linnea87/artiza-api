361a63ebe5e05fbd8f9328d36ae94cb27d9d964f
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-15 09:48:24 +0000

    update profile detail

--- profiles/views.py
+++ profiles/views.py
@@ -15,16 +15,17 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
     def get_object(self, pk):
         try:
-            profile=Profile.objects.get(pk=pk)
+            profile = Profile.objects.get(pk=pk)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer=ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
     
     def put(self, request, pk):
