8544b1c3727f733692f4e7ae9ac090ff3d0b3342
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-15 17:16:36 +0000

    added image_filter choice field and image validation

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# Class provided by DRF-API walkthrough.
 class Post(models.Model):
     """
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,8 +32,15 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_ml2gkn', blank=True
     )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
 
     class Meta:
+        """
+        Order posts by date created.
+        Display by most recent first.
+        """
         ordering = ['-created_at']
 
     def __str__(self):
