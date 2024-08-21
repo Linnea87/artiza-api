305152deb8a82181957c0d24cf312b305720d9c8
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-21 15:11:19 +0000

    add CategoryList view and urls

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryList(generics.ListAPIView):
+    """
+    API view to list all categories.
+
+    Allows filtering by name.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
