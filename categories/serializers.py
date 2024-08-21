7e13167bb4a21ab0f18e4140986940fae11b6493
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-21 14:45:09 +0000

    add category and category detail serializer

---
+++ categories/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'created_at']
+
+class CategoryDetailSerializer(CategorySerializer):
+    category_id = serializers.ReadOnlyField(source='id')
+
+    class Meta:
+        model = Category
+        fileds = ['id', 'name', 'created_at', 'category_id']
