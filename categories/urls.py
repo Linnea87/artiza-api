66ac8e24a19d2ff1c01720a283a7a51ee8ba7965
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-22 11:41:21 +0000

    update categories urls

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from categories import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view()),
     path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('api/', include(router.urls)),
 ]
