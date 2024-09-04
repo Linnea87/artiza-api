70234e0749bb5d426ac2b3c96c022f7cd33e4ca8
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-09-04 11:07:54 +0000

    add api root

--- categories/urls.py
+++ categories/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import CategoryList, CategoryDetail
 
+router = DefaultRouter()
 
 urlpatterns = [
     path('categories/', CategoryList.as_view()),
     path('categories/<int:pk>/', CategoryDetail.as_view()),
+    path('api/', include(router.urls)),
 ]
