76d4e038c034c7087de0fb44f8002aaea0765e7b
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-09-04 17:50:55 +0000

    removed syntax

--- posts/models.py
+++ posts/models.py
@@ -12,9 +12,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_ml2gkn', blank=True
-    )
-   
+        upload_to='images/', default='../default_post_ml2gkn', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE) 
 
     class Meta:
