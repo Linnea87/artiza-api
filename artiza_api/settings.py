8615e753561983ba3ca142a3e20eddce2e3efdd8
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-09-05 11:29:55 +0000

    deployment

--- artiza_api/settings.py
+++ artiza_api/settings.py
@@ -62,8 +62,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEBUG' in os.environ
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
+
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
